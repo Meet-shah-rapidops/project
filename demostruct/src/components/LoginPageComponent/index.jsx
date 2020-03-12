e35ff457505525433b20c3e29b6e0a5de2c99a95
Hardik Motwani <motwanihardik@gmail.com>
2020-03-12 21:52:03 +0530

    Register and Login page

--- demostruct/src/components/LoginPageComponent/index.jsx
+++ demostruct/src/components/LoginPageComponent/index.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import {
+  EuiForm,
+  EuiFormRow,
   EuiButton,
   EuiFieldText,
   EuiFieldPassword,
-  EuiLink
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiTitle,
+  EuiLink,
+  EuiFlexGroup,
+  EuiFormLabel
 } from "@elastic/eui";
 
 class LoginPageComponent extends Component {
@@ -24,36 +35,81 @@ class LoginPageComponent extends Component {
 
   render() {
     return (
-      <div className="bg-image">
-        <div className="LoginPage">
-          <h1 className="loginName">Login Here</h1>
-          <EuiFieldText
-            className="email"
-            compressed
-            placeholder="Enter your email"
-            value={this.state.value}
-            onChange={this.onChange}
-            aria-label="Use aria labels when no actual label is in use"
-          />
-          <EuiFieldPassword
-            className="password"
-            compressed
-            placeholder="Enter your password"
-            value={this.state.value}
-            onChange={this.onChange}
-            aria-label="Use aria labels when no actual label is in use"
-          />
-          <Link to="/adminDashboard">
-            <EuiButton className="loginBtn">Login</EuiButton>
-            <br />
-          </Link>
+      // <div className="bg-image">
+      //   <div className="LoginPage">
+      //     <h1 className="loginName">Login Here</h1>
+      //     <EuiFieldText
+      //       className="email"
+      //       compressed
+      //       placeholder="Enter your email"
+      //       value={this.state.value}
+      //       onChange={this.onChange}
+      //       aria-label="Use aria labels when no actual label is in use"
+      //     />
+      //     <EuiFieldPassword
+      //       className="password"
+      //       compressed
+      //       placeholder="Enter your password"
+      //       value={this.state.value}
+      //       onChange={this.onChange}
+      //       aria-label="Use aria labels when no actual label is in use"
+      //     />
+      //     <Link to="/adminDashboard">
+      //       <EuiButton className="loginBtn">Login</EuiButton>
+      //       <br />
+      //     </Link>
 
-          {/* <a href= '#'>Forget Password</a> */}
-          <EuiLink color="secondary" href="#">
-            Forget Password
-          </EuiLink>
-        </div>
-      </div>
+      //     {/* <a href= '#'>Forget Password</a> */}
+      //     <EuiLink color="secondary" href="#">
+      //       Forget Password
+      //     </EuiLink>
+      //   </div>
+      // </div>
+      <div className="LoginPage">
+        <EuiPage>
+          <EuiPageBody>
+            <EuiPageContent verticalPosition="center" horizontalPosition="center">
+              <EuiPageContentHeader>
+                <EuiPageContentHeaderSection>
+                  <EuiTitle>
+                    <h2>Welcome</h2>
+                  </EuiTitle>
+                </EuiPageContentHeaderSection>
+              </EuiPageContentHeader>
+              <EuiPageContentBody>
+                <EuiForm style={{ width: 350 }}>
+
+                  <EuiFormRow label="Email" value={this.state.value} onChange={this.onChange}>
+                    <EuiFieldText icon="email" />
+                  </EuiFormRow>
+
+                  <EuiFormRow label="Password" helpText="Must include one number and one symbol" value={this.state.value} onChange={this.onChange}>
+                    <EuiFieldPassword icon="lock" />
+                  </EuiFormRow>
+
+                  <EuiFormRow id="forgot">
+                    <EuiLink id="link">Forgot password?</EuiLink>
+                  </EuiFormRow>
+
+                  <Link to="/adminDashboard">
+                    <EuiButton type="submit" fill onClick={() => window.alert('Button clicked')}>
+                      Login
+                  </EuiButton>
+                  </Link>
+
+                  <EuiFlexGroup id="register">
+                    <EuiFormLabel id="label">Not registered?</EuiFormLabel>
+                    <Link to="/register">
+                      <EuiLink id="link">Create an account</EuiLink>
+                    </Link>
+                  </EuiFlexGroup>
+
+                </EuiForm>
+              </EuiPageContentBody>
+            </EuiPageContent>
+          </EuiPageBody>
+        </EuiPage>
+      </div >
     );
   }
 }
--- demostruct/src/components/RegisterComponent/index.jsx
+++ demostruct/src/components/RegisterComponent/index.jsx
@@ -1,12 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    EuiForm,
+    EuiFlexGroup,
+    EuiFlexItem,
+    EuiFormRow,
+    EuiButton,
+    EuiFieldText,
+    EuiFieldPassword,
+    EuiPage,
+    EuiPageBody,
+    EuiPageContent,
+    EuiPageContentBody,
+    EuiPageContentHeader,
+    EuiPageContentHeaderSection,
+    EuiTitle,
+    EuiButtonEmpty
+} from '@elastic/eui';
+import { EuiFormLabel } from '@elastic/eui';
+import { EuiLink } from '@elastic/eui';
 
 class RegisterComponent extends Component {
     render() {
         return (
-            <div>
-                Register Page
+            <div className="RegisterPage">
+                <EuiPage>
+                    <EuiPageBody>
+                        <EuiPageContent verticalPosition="center" horizontalPosition="center">
+                            <EuiPageContentHeader>
+                                <EuiPageContentHeaderSection>
+                                    <EuiTitle>
+                                        <h2>Sign Up</h2>
+                                    </EuiTitle>
+                                </EuiPageContentHeaderSection>
+                            </EuiPageContentHeader>
+                            <EuiPageContentBody>
+                                <EuiForm style={{ width: 350 }}>
+
+                                    <EuiFormRow label="Username">
+                                        <EuiFieldText icon="user" placeholder="John" />
+                                    </EuiFormRow>
+
+                                    <EuiFormRow label="Email">
+                                        <EuiFieldText icon="email" />
+                                    </EuiFormRow>
+
+                                    <EuiFormRow label="Password" helpText="Must include one number and one symbol">
+                                        <EuiFieldPassword icon="lock" />
+                                    </EuiFormRow>
+
+                                    <EuiFormRow label="Confirm Password" helpText="Must include one number and one symbol">
+                                        <EuiFieldPassword icon="lock" />
+                                    </EuiFormRow>
+
+                                    <Link to="/login">
+                                        <EuiButton id="registerBtn" type="submit" fill onClick={() => window.alert('Button clicked')}>
+                                            Register
+                                        </EuiButton>
+                                    </Link>
+
+                                    <EuiFlexGroup id="login">
+                                        <EuiFormLabel id="label">Already have an account?</EuiFormLabel>
+                                        <Link to="/login">
+                                            <EuiLink id="link">Log in</EuiLink>
+                                        </Link>
+                                    </EuiFlexGroup>
+
+                                </EuiForm>
+                            </EuiPageContentBody>
+                        </EuiPageContent>
+                    </EuiPageBody>
+                </EuiPage>
             </div>
-        )
+        );
     }
 }
 
